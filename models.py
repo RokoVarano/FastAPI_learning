4d31d3306ffc0995e0b7586ae42e2bcacba6b7da
Rodrigo <rodrigo.ibaceta01@gmail.com>
2022-01-03 18:34:24 -0300

    Tutorial complete

---
+++ models.py
@@ -0,0 +1,13 @@
+from database import Base
+from sqlalchemy import String, Boolean, Integer, Column, Text
+
+class Item(Base):
+    __tablename__ = 'items'
+    id=Column(Integer, primary_key=True)
+    name=Column(String(255), nullable=False, unique=True)
+    price=Column(Integer, nullable=False)
+    description=Column(Text, nullable=True)
+    on_offer=Column(Boolean, default=False)
+
+    def __repr__(self) -> str:
+        return f"<Item name={self.name} price={self.price}>"
